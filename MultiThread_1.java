def9c57224c504b5773cfc45dfab75449313738a
PilMugi <142198578+PilMugi@users.noreply.github.com>
2023-09-04 16:02:47 +0900

    Add files via upload
    2023.09.04

---
+++ MultiThread_1.java
@@ -0,0 +1,38 @@
+package s0904;
+
+public class MultiThread_1 {
+
+	public static void main(String[] args) {
+		
+		MyThread my = new MyThread();
+		my.start();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.print("@ ");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
+
+class MyThread extends Thread{
+	@Override
+	public void run(){
+		for(int i = 0; i < 10; i++) {
+			System.out.print("* ");
+			System.out.print(". ");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
